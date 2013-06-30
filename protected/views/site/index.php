880f681bdfce657219ad3955e3a6de4d08d6d37c
Ligreman <ligreman@users.noreply.github.com>
2013-06-30 15:22:23 +0000

    Autenticación de usuarios completa

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,8 +6,6 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
-
 <p>You may change the content of this page by modifying the following two files:</p>
 <ul>
 	<li>View file: <code><?php echo __FILE__; ?></code></li>
