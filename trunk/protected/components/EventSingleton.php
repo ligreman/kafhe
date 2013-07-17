0241d42c643ce1a8abc6c42c6285f0b48806c50d
ligreman@gmail.com <ligreman@gmail.com@0186b76e-ba99-af01-771a-274165256fcf>
2013-07-17 19:12:26 +0000

    Notificaciones (parcial). Ver con usuario test1.

---
+++ trunk/protected/components/EventSingleton.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * EventSingleton para el estado de los eventos actuales 
+ */
+class EventSingleton extends CApplicationComponent
+{
+	private $_model = null;
+
+	public function setModel($id)
+    {
+        $this->_model = Event::model()->findByPk($id);
+    }
+
+    //Esta función la coge automáticamente
+    public function getModel()
+    {
+        if (!$this->_model)
+        {
+            $type = 'desayuno'; //Si no hay un modelo cargado, cargo el modelo de desayuno por defecto
+            Yii::log('Modelo Event', 'info', 'aa.yy.zz');
+
+            if (!isset(Yii::app()->user->group_id))
+                return null;
+
+            //Aquí se podría mirar la sesión también para tomar de allí el evento actualmente cargado. Yii::app()->session['var'] = 'value';
+            if (isset($_GET['event_type'])) {
+                //tipo indicado en el GET
+                $type=htmlentities($_GET['event_type']);
+            }
+
+            //Cargo el último evento por fecha, del tipo seleccionado
+            $criteria = New CDbCriteria;
+            $criteria->condition = 'group_id=:groupId AND type=:type';
+            $criteria->params = array(':groupId'=>Yii::app()->user->group_id, ':type'=>$type);
+            $criteria->order = 'date DESC';
+            $criteria->limit = '1';
+
+            $this->_model = Event::model()->find($criteria);
+        }
+
+        return $this->_model;
+    }
+
+    /*public function ppp()
+    {
+        return 'popo';
+    }*/
+}
---
+++ trunk/protected/models/Drink.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * This is the model class for table "drink".
+ *
+ * The followings are the available columns in table 'drink':
+ * @property string $id
+ * @property string $name
+ * @property integer $price
+ * @property integer $ito
+ */
+class Drink extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return Drink the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'drink';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('name, price', 'required'),
+			array('price, ito', 'numerical', 'integerOnly'=>true),
+			array('name', 'length', 'max'=>50),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('id, name, price, ito', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'name' => 'Name',
+			'price' => 'Price',
+			'ito' => 'Ito',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id,true);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('price',$this->price);
+		$criteria->compare('ito',$this->ito);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
