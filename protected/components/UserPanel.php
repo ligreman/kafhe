891332a2469e8d5f3c8661b38eaa703f0c2dfebc
cgoo85@gmail.com <cgoo85@gmail.com@0186b76e-ba99-af01-771a-274165256fcf>
2013-08-03 19:02:03 +0000

    Nuevas imagenes para las habilidades de la BETA: hidratar, disimular, cria de gungubos y relanzamiento
    Maquetadas las habilidades junto con el userPanel y controlada su presentación con jQuery (aparentemente funciona)

--- protected/components/UserPanel.php
+++ protected/components/UserPanel.php
@@ -3,7 +3,7 @@ class UserPanel extends CWidget {
 
     public function run() {
         $data['user'] = User::model()->findByPk(Yii::app()->user->id);
-
+        $data['skills'] = Skill::model()->findAll(array('order'=>'category, type, name'));
 
         $this->render('userPanel',$data);
     }
