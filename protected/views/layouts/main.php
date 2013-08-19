2ed3ec9fa71f3a23e01c20fd69b44ebfbbe6be53
cgoo85@gmail.com <cgoo85@gmail.com@0186b76e-ba99-af01-771a-274165256fcf>
2013-08-19 13:18:42 +0000

    Maquetado el pedido del Historico de eventos.
    Se muestra el estado del jugador.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -5,7 +5,6 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
 
 	<!-- blueprint CSS framework -->
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
