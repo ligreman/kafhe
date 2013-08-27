6dd2035d702143f647e27f802e602641a697467c
Ligreman <ligreman@users.noreply.github.com>
2013-08-27 18:45:29 +0000

    Arreglados algunos bugs.

--- protected/commands/CronCommand.php
+++ protected/commands/CronCommand.php
@@ -37,139 +37,143 @@
 */
 
 class CronCommand extends CConsoleCommand {
-	public $global_param = true;
-	
-	//Ejecuta esto si no se pasa ninguna acción
-	/*public function actionIndex ()
-	{
-		echo "Index";
-		return 0;
-	}
-	
-	//Acción por defecto: index
+    public $global_param = true;
+
+    //Ejecuta esto si no se pasa ninguna acción
+    /*public function actionIndex ()
+    {
+        echo "Index";
+        return 0;
+    }
+
+    //Acción por defecto: index
     public function actionParams ($param1, $param2='default', array $param3)
-	{
+    {
         // here we are doing what we need to do
-		echo "ok";
-		return 0;
+        echo "ok";
+        return 0;
     }*/
 
 
-	//NOTA no necesitaré comprobar modificadores ya que lo compruebo en cada carga de página y al regenerar tueste en el Cron
-	
-	
-	/*
-	*	Regenera el tueste del usuario $userId (ID User). Si $userId es nulo regenera el tueste de todos los usuarios de grupos activos.
-	*/
-	public function actionRegenerarTueste($userId=null)	
-	{
-		echo "Compruebo caducidad de modificadores.\n";
-		Yii::app()->modifier->checkModifiersExpiration();
-		
-		echo "Iniciando regeneracion.\n";
-		if ($userId === null) {
-			echo "Regenero a todos los usuarios.\n";
-			$grupos = Group::model()->findAll(array('condition'=>'active=1'));
-			
-			foreach($grupos as $grupo) {
-				echo "  Usuarios del grupo ".$grupo->name." (".$grupo->id.").\n";
-				$usuarios = User::model()->findAll(array('condition'=>'group_id=:groupId', 'params'=>array(':groupId'=>$grupo->id)));
-				
-				foreach ($usuarios as $usuario) {					
-					$regenerado = Yii::app()->tueste->getTuesteRegenerado($usuario);
-					
-					if ($regenerado !== false) {
-						$usuario->ptos_tueste = min( intval(Yii::app()->config->getParam('maxTuesteUsuario')), ($usuario->ptos_tueste+$regenerado) );
-						$usuario->last_regen_timestamp = date('Y-m-d H:i:s');
-						if (!$usuario->save())
-							echo "** ERROR al guardar el usuario (".$usuario->id.") regenerando su tueste.\n";
-						
-						echo "    Usuario ".$usuario->username." - Tueste regenerado: " . $regenerado."\n";
-					} else
-						echo "    Usuario ".$usuario->username." - Todavia no puede regenerar tueste.\n";
-				}
-			}
-			
-		} else {
-			//Regenero a un usuario concreto
-			$usuario = User::model()->findByPk($userId);
-			$regenerado = Yii::app()->tueste->getTuesteRegenerado($usuario);
-			
-			if ($regenerado !== false) {
-				$usuario->ptos_tueste = min( intval(Yii::app()->config->getParam('maxTuesteUsuario')), ($usuario->ptos_tueste+$regenerado) );
-				$usuario->last_regen_timestamp = date('Y-m-d H:i:s');
-				if (!$usuario->save())
-					echo "** ERROR al guardar el usuario (".$usuario->id.") regenerando su tueste.\n";
-				
-				echo "    Usuario ".$usuario->username." - Tueste regenerado: " . $regenerado."\n";
-			} else
-				echo "    Usuario ".$usuario->username." - Todavia no puede regenerar tueste.\n";
-		}
-		
-		/*$time = time();
-		echo date('d-m-Y H:m:s')."\n";
-		echo $time."\n";
-		echo date('d-m-Y H:m:s', $time)."\n";
-		echo print_r(getdate($time),true)."\n";
-		echo print_r(getdate(strtotime(date('d-m-Y H:i:s'))),true);*/
-		return 0;
-	}
-	
-	/*
-	*	Cría gungubos cada hora para el bando
-	*/
-	public function actionCriarGungubos($eventId=null)	
-	{
-		echo "Compruebo caducidad de modificadores.\n";
-		Yii::app()->modifier->checkModifiersExpiration();
-		
-		if ($eventId === null) {
-			//Para todos los eventos de estado "iniciado" (1)
-			$events = Event::model()->findAll(array('condition'=>'status=:status', 'params'=>array(':status'=>Yii::app()->params->statusIniciado)));
-			if ($events != null) {
-				foreach($events as $event) {
-					$criados = Yii::app()->gungubos->getGungubosCriados($event);			
-					if ($criados !== false) {
-						//Guardo el evento
-						$event->gungubos_kafhe += $criados['kafhe'];
-						$event->gungubos_achikhoria += $criados['achikhoria'];
-						$event->last_gungubos_timestamp = date('Y-m-d H:i:s');
-						
-						if (!$event->save())
-							echo "** ERROR al guardar el evento (".$event->id.") criando gungubos.\n";
-						
-						echo "Criados ".$criados['kafhe']." gungubos para Kafhe. Evento ".$event->id.".\n";
-						echo "Criados ".$criados['achikhoria']." gungubos para Achikhoria. Evento ".$event->id.".\n";
-					} else
-						echo "Todavia no puede criar gungubos.\n";
-				}
-			}
-		} else {		
-			//Para el evento concreto. Compruebo que está en estado (1) antes
-			$event = Event::model()->findByPk($eventId);
-			if ($event->status != Yii::app()->params->statusIniciado) return 0;
-			
-			$criados = Yii::app()->gungubos->getGungubosCriados($event);			
-			if ($criados !== false) {
-				//Guardo el evento
-				$event->gungubos_kafhe += $criados['kafhe'];
-				$event->gungubos_achikhoria += $criados['achikhoria'];
-				$event->last_gungubos_timestamp = date('Y-m-d H:i:s');
-				
-				if (!$event->save())
-					echo "** ERROR al guardar el evento (".$event->id.") criando gungubos.\n";
-				
-				echo "Criados ".$criados['kafhe']." gungubos para Kafhe.\n";
-				echo "Criados ".$criados['achikhoria']." gungubos para Achikhoria.\n";
-			} else
-				echo "Todavía no puede criar gungubos.\n";
-		}
-				
-		return 0;
-	}
+    //NOTA no necesitaré comprobar modificadores ya que lo compruebo en cada carga de página y al regenerar tueste en el Cron
 
 
-	public function actionGenerateRanking() {
+    /*
+    *	Regenera el tueste del usuario $userId (ID User). Si $userId es nulo regenera el tueste de todos los usuarios de grupos activos.
+    */
+    public function actionRegenerarTueste($userId=null)
+    {
+        echo "Compruebo caducidad de modificadores.\n";
+        Yii::app()->modifier->checkModifiersExpiration();
+
+        echo "Iniciando regeneracion.\n";
+        if ($userId === null) {
+            echo "Regenero a todos los usuarios.\n";
+            $grupos = Group::model()->findAll(array('condition'=>'active=1'));
+
+            foreach($grupos as $grupo) {
+                echo "  Usuarios del grupo ".$grupo->name." (".$grupo->id.").\n";
+                $usuarios = User::model()->findAll(array('condition'=>'group_id=:groupId', 'params'=>array(':groupId'=>$grupo->id)));
+
+                foreach ($usuarios as $usuario) {
+                    $regenerado = Yii::app()->tueste->getTuesteRegenerado($usuario);
+
+                    if ($regenerado !== false) {
+                        //Si ya estoy al máximo de tueste cambio mi estado a Criador, si soy Cazador
+                        if ($usuario->ptos_tueste>=Yii::app()->config->getParam('maxTuesteUsuario')  &&  $usuario->status==Yii::app()->params->statusCazador)
+                            $usuario->status = Yii::app()->params->statusCriador;
+
+                        $usuario->ptos_tueste = min( intval(Yii::app()->config->getParam('maxTuesteUsuario')), ($usuario->ptos_tueste+$regenerado) );
+                        $usuario->last_regen_timestamp = date('Y-m-d H:i:s');
+                        if (!$usuario->save())
+                            echo "** ERROR al guardar el usuario (".$usuario->id.") regenerando su tueste.\n";
+
+                        echo "    Usuario ".$usuario->username." - Tueste regenerado: " . $regenerado."\n";
+                    } else
+                        echo "    Usuario ".$usuario->username." - Todavia no puede regenerar tueste.\n";
+                }
+            }
+
+        } else {
+            //Regenero a un usuario concreto
+            $usuario = User::model()->findByPk($userId);
+            $regenerado = Yii::app()->tueste->getTuesteRegenerado($usuario);
+
+            if ($regenerado !== false) {
+                $usuario->ptos_tueste = min( intval(Yii::app()->config->getParam('maxTuesteUsuario')), ($usuario->ptos_tueste+$regenerado) );
+                $usuario->last_regen_timestamp = date('Y-m-d H:i:s');
+                if (!$usuario->save())
+                    echo "** ERROR al guardar el usuario (".$usuario->id.") regenerando su tueste.\n";
+
+                echo "    Usuario ".$usuario->username." - Tueste regenerado: " . $regenerado."\n";
+            } else
+                echo "    Usuario ".$usuario->username." - Todavia no puede regenerar tueste.\n";
+        }
+
+        /*$time = time();
+        echo date('d-m-Y H:m:s')."\n";
+        echo $time."\n";
+        echo date('d-m-Y H:m:s', $time)."\n";
+        echo print_r(getdate($time),true)."\n";
+        echo print_r(getdate(strtotime(date('d-m-Y H:i:s'))),true);*/
+        return 0;
+    }
+
+    /*
+    *	Cría gungubos cada hora para el bando
+    */
+    public function actionCriarGungubos($eventId=null)
+    {
+        echo "Compruebo caducidad de modificadores.\n";
+        Yii::app()->modifier->checkModifiersExpiration();
+
+        if ($eventId === null) {
+            //Para todos los eventos de estado "iniciado" (1)
+            $events = Event::model()->findAll(array('condition'=>'status=:status', 'params'=>array(':status'=>Yii::app()->params->statusIniciado)));
+            if ($events != null) {
+                foreach($events as $event) {
+                    $criados = Yii::app()->gungubos->getGungubosCriados($event);
+                    if ($criados !== false) {
+                        //Guardo el evento
+                        $event->gungubos_kafhe += $criados['kafhe'];
+                        $event->gungubos_achikhoria += $criados['achikhoria'];
+                        $event->last_gungubos_timestamp = date('Y-m-d H:i:s');
+
+                        if (!$event->save())
+                            echo "** ERROR al guardar el evento (".$event->id.") criando gungubos.\n";
+
+                        echo "Criados ".$criados['kafhe']." gungubos para Kafhe. Evento ".$event->id.".\n";
+                        echo "Criados ".$criados['achikhoria']." gungubos para Achikhoria. Evento ".$event->id.".\n";
+                    } else
+                        echo "Todavia no puede criar gungubos.\n";
+                }
+            }
+        } else {
+            //Para el evento concreto. Compruebo que está en estado (1) antes
+            $event = Event::model()->findByPk($eventId);
+            if ($event->status != Yii::app()->params->statusIniciado) return 0;
+
+            $criados = Yii::app()->gungubos->getGungubosCriados($event);
+            if ($criados !== false) {
+                //Guardo el evento
+                $event->gungubos_kafhe += $criados['kafhe'];
+                $event->gungubos_achikhoria += $criados['achikhoria'];
+                $event->last_gungubos_timestamp = date('Y-m-d H:i:s');
+
+                if (!$event->save())
+                    echo "** ERROR al guardar el evento (".$event->id.") criando gungubos.\n";
+
+                echo "Criados ".$criados['kafhe']." gungubos para Kafhe.\n";
+                echo "Criados ".$criados['achikhoria']." gungubos para Achikhoria.\n";
+            } else
+                echo "Todavía no puede criar gungubos.\n";
+        }
+
+        return 0;
+    }
+
+
+    public function actionGenerateRanking() {
         //Cojo los grupos
         $groups = Group::model()->findAll();
 
@@ -209,11 +213,17 @@ class CronCommand extends CConsoleCommand {
             //Por último, guardo el ranking de este grupo (los 10 primeros sólo)
             $connection = Yii::app()->db;
 
-            $values = array();
-            for ($i=0; $i<10; $i++) {
-				if (!isset($ranking[$i])) break; //Paro si no hay más
-				
+            $values = $ya_ha_salido = array();
+            $cuenta = 0;
+            for ($i=0; $i<count($ranking); $i++) {
+                if ($cuenta>=10) break; //Paro si no hay más
+
+                if (in_array($ranking[$i]->id.'#'.$ranking[$i]->rank, $ya_han_salido)) continue;
+                else $ya_ha_salido[] = $ranking[$i]->id.'#'.$ranking[$i]->rank; //Evito repetidos
+
                 $values[] = "(".$ranking[$i]->id.", ".$ranking[$i]->rank.", '".$ranking[$i]->date."')";
+
+                $cuenta++;
             }
 
             $sql="INSERT INTO ranking (user_id, rank, date) VALUES ".implode(',',$values);
@@ -221,6 +231,6 @@ class CronCommand extends CConsoleCommand {
             $command->execute();
         }
 
-	    return 0;
-	}
+        return 0;
+    }
 }
