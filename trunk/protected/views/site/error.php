6cfdd69befda9fd12a8d1be7a430e5b6b8f62bed
cgoo85@gmail.com <cgoo85@gmail.com@0186b76e-ba99-af01-771a-274165256fcf>
2013-08-28 16:26:47 +0000

    Página de error 404 e incluido el tfno del Espiral en la pag de pedido

--- trunk/protected/views/site/error.php
+++ trunk/protected/views/site/error.php
@@ -5,9 +5,13 @@
 $this->pageTitle=Yii::app()->name . ' - Error';
 ?>
 <div id="menuContent" class="paddedContent">
-    <h2>Error <?php echo $code; ?></h2>
+    <h2 class="error<?php echo $code; ?>">Error <?php echo $code; ?></h2>
 
-    <div class="error">
-        <?php echo CHtml::encode($message); ?>
+    <?php if($code == "404"): ?>
+    <div class="error404">
+        <p>El Gran Omelettus aún no ha permitido a los humanos venir aquí.</p>
+        <?php echo CHtml::image(Yii::app()->getBaseUrl(true).'/images/404.png','');?>
+        <?php //echo CHtml::encode($message); ?>
     </div>
+    <?php endif;?>
 </div>
