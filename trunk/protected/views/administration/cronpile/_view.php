f4064e5b8731d5affea00e295154f64861712d67
ligreman@gmail.com <ligreman@gmail.com@0186b76e-ba99-af01-771a-274165256fcf>
2013-09-04 21:37:04 +0000

    Relauncher_id en escaquearse.
    Cronpile en administración.

---
+++ trunk/protected/views/administration/cronpile/_view.php
@@ -0,0 +1,16 @@
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id),array('view','id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('operation')); ?>:</b>
+	<?php echo CHtml::encode($data->operation); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('params')); ?>:</b>
+	<?php echo CHtml::encode($data->params); ?>
+	<br />
+
+
+</div>
