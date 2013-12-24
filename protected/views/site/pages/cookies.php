944c3d60eebb6112c751d36f95c193a444b9ba24
Ligreman <ligreman@users.noreply.github.com>
2013-12-24 15:55:28 +0000

    Uso de cookies

---
+++ protected/views/site/pages/cookies.php
@@ -0,0 +1,95 @@
+<div class="paddedContent page">
+    <?php
+    /* @var $this SiteController */
+
+    $this->pageTitle=Yii::app()->name . ' - Uso de cookies';
+
+    ?>
+    <div class="ATodaPagina">
+        <!--SUB CONTENIDO-->
+        <h1>Política de cookies</h1>
+
+        <p>Este web utiliza cookies propias para ofrecerle una mejor experiencia y servicio. Al navegar o utilizar nuestros servicios el usuario acepta el uso que hacemos de las cookies. Sin embargo, el usuario tiene la opción de impedir la generación de cookies y la eliminación de las mismas mediante la selección de la correspondiente opción en su navegador. En caso de bloquear el uso de cookies en su navegador es posible que algunos servicios o funcionalidades de la página web no estén disponibles.</p>
+
+        <p><strong>¿Qué es una cookie?</strong></p>
+
+        <p>Una cookie es un pequeño fragmento de texto que los sitios web envían al navegador y que se almacenan en el terminal del usuario, el cual puede ser un ordenador personal, un teléfono móvil, una tableta, etc. Estos archivos permiten que el sitio web recuerde información sobre su visita, como el idioma y las opciones preferidas, lo que puede facilitar su próxima visita y hacer que el sitio resulte más útil al personalizar su contenido. Las cookies desempeñan un papel muy importante, al mejorar la experiencia del uso de la web.</p>
+
+        <p><strong>¿Cómo se utilizan las cookies?</strong></p>
+
+        <p>Al navegar por este portal web el usuario está aceptando que se puedan instalar cookies en su terminal y que nos permiten conocer la siguiente información:</p>
+
+        <ul>
+            <li>El 'login' del usuario para mantener la sesión activa en la web.</li>
+        </ul>
+
+        <p><strong>Tipos de cookies utilizadas</strong></p>
+
+        <p>Esta web utiliza cookies temporales de sesión. Las cookies de sesión almacenan datos únicamente mientras el usuario accede a la web y las cookies permanentes almacenan los datos en el terminal para que sean accedidos y utilizados en más de una sesión.</p>
+
+        <p>Según la finalidad para la que se traten los datos obtenidos a través de las cookies, la web puede utilizar:</p>
+
+        <p><strong><em> cookies técnicas </em></strong></p>
+
+        <p>Son aquéllas que permiten al usuario la navegación a través de la página web o aplicación y la utilización de las diferentes opciones o servicios que en ella existen. Por ejemplo, controlar el tráfico y la comunicación de datos, identificar la sesión, acceder a las partes web de acceso restringido, recordar los elementos que integran un pedido, realizar la solicitud de inscripción o participación en un evento, utilizar elementos de seguridad durante la navegación y almacenar contenidos para la difusión de videos o sonido.</p>
+
+        <p><em><strong>cookies de personalización </strong></em></p>
+
+        <p>Son aquéllas que permiten al usuario acceder al servicio con algunas características de carácter general predefinidas en su terminal o que el propio usuario defina. Por ejemplo, el idioma, el tipo de navegador a través del cual accede al servicio, el diseño de contenidos seleccionado, geolocalización del terminal y la configuración regional desde donde se accede al servicio.</p>
+
+        <p><em><strong>cookies publicitarias</strong></em></p>
+
+        <p>Son aquéllas que permiten la gestión eficaz de los espacios publicitarios que se han incluido en la página web o aplicación desde la que se presta el servicio. Permiten adecuar el contenido de la publicidad para que esta sea relevante para el usuario y para evitar mostrar anuncios que el usuario ya haya visto.</p>
+
+        <p><strong><em> cookies de análisis estadístico </em></strong></p>
+
+        <p>Son aquéllas que permiten realizar el seguimiento y análisis del comportamiento de los usuarios en los sitios web. La información recogida mediante este tipo de cookies se utiliza en la medición de la actividad de los sitios web, aplicación o plataforma y para la elaboración de perfiles de navegación de los usuarios de dichos sitios, con el fin de introducir mejoras en el servicio en función de los datos de uso que hacen los usuarios.</p>
+
+        <p><em><strong>cookies de terceros</strong></em></p>
+
+        <p>En algunas páginas web se pueden instalar cookies de terceros que permiten gestionar y mejorar los servicios ofrecidos. Como por ejemplo, servicios estadísticos de Google Analytics y Comscore.</p>
+
+        <p><strong>¿Cómo administrar cookies en el navegador?</strong></p>
+
+        <p>El usuario tiene la opción de permitir, bloquear o eliminar las cookies instaladas en su equipo mediante la configuración de las opciones del navegador instalado en su terminal:</p>
+
+        <p>Para ajustar los permisos relacionados con las cookies en el navegador <strong>Google Chrome</strong>:</p>
+
+        <ul>
+            <li>Hacer clic en el menú situado en la barra de herramientas.</li>
+            <li>Seleccionar <strong>Configuración</strong>.</li>
+            <li>Hacer clic en <strong>Mostar opciones avanzadas</strong>.</li>
+            <li>En la sección 'Privacidad' hacer clic en el botón <strong>Configuración de contenido</strong>.</li>
+            <li>En la sección de cookies se pueden configurar las opciones.</li>
+        </ul>
+
+        <p><a href="https://support.google.com/chrome/answer/95647?hl=es" target="_blank" rel="nofollow">Más información sobre Google Chrome</a></p>
+
+        <p>Para ajustar los permisos relacionados con las cookies en el navegador <strong>Mozilla Firefox</strong>:</p>
+
+        <ul>
+            <li>En la parte superior de la ventana de Firefox hacer clic en el menú <strong>Herramientas</strong>.</li>
+            <li>Seleccionar <strong>Opciones.</strong></li>
+            <li>Seleccionar el panel <strong> Privacidad</strong>.</li>
+            <li>En la opción Firefox podrá elegir <strong>Usar una configuración personalizada para el historial </strong> para configurar las opciones.</li>
+        </ul>
+
+        <p><a href="https://support.mozilla.org/es/kb/cookies-informacion-que-los-sitios-web-guardan-en-?esab=a&amp;s=adminstracion+de+cookies&amp;r=2&amp;as=s" target="_blank" rel="nofollow">Más información sobre Mozilla Firefox</a></p>
+
+        <p>Para ajustar los permisos relacionados con las cookies en el navegador <strong>Internet Explorer 9</strong>:</p>
+
+        <ul>
+            <li>En la parte superior de la ventana de Internet Explorer hacer clic en el menú <strong>Herramientas.</strong></li>
+            <li>Seleccionar la pestaña de <strong>Seguridad</strong> y utilizar la opción <strong>Eliminar el historial de exploración</strong> para eliminar las cookies. Activar la casilla <strong> cookies </strong> y, a continuación, hacer clic en <strong> Eliminar</strong>.</li>
+            <li>Seleccionar la pestaña de <strong>Seguridad</strong> y acceder a <strong>Configuración</strong>. Mover el control deslizante totalmente hacia arriba para bloquear todas las cookies o totalmente hacia abajo para permitir todas las cookies. Tras ello, hacer clic en <strong>Aceptar</strong>.</li>
+        </ul>
+
+        <p><a href="http://windows.microsoft.com/es-es/windows7/how-to-manage-cookies-in-internet-explorer-9" target="_blank" rel="nofollow">Más información sobre Internet Explorer 9</a></p>
+
+        <p>En caso de bloquear el uso de cookies en su navegador es posible que algunos servicios o funcionalidades de la página web no estén disponibles.</p>
+
+        <p>En algunos navegadores se pueden configurar reglas específicas para administrar cookies por sitio web, lo que ofrece un control más preciso sobre la privacidad. Esto significa que se puede inhabilitar cookies de todos los sitios salvo de aquellos en los que se confíe.</p>
+
+
+    </div>
+</div>
