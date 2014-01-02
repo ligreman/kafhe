dd360a7ed4a1e94298aa7e374464355a2b207081
ligreman@gmail.com <ligreman@gmail.com@0186b76e-ba99-af01-771a-274165256fcf>
2014-01-02 22:44:37 +0000

    Barra de fama.

--- trunk/protected/config/console.php
+++ trunk/protected/config/console.php
@@ -171,7 +171,7 @@ return array(
 
 
         //Otros
-        'sideNames'=>array('kafhe'=>'Kafhe', 'achikhoria'=>'Achikhoria', 'libre'=>'Teh'),
+        'sideNames'=>array('kafhe'=>'Kafhe', 'achikhoria'=>'Achikhoria', 'libre'=>'Têh'),
         'userStatusNames'=>array(0=>'Inactivo', 1=>'Alborotador', 2=>'Combatiente', 3=>'Baja', 4=>'Espectador', 5=>'Libertador'),
         'eventStatusNames'=>array(0=>'Cerrada', 1=>'Fama', 2=>'Batalla', 3=>'Finalizada', 4=>'En calma', 5=>'Preparativos'),
 
