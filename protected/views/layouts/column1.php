b6c6267516db7ebaf2946438f3a0cba034c93837
Ligreman <ligreman@users.noreply.github.com>
2014-01-03 20:09:21 +0000

    Se muestran las notificaciones del corral en un panel derecho

--- protected/views/layouts/column1.php
+++ protected/views/layouts/column1.php
@@ -1,6 +1,4 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
-<div id="content">
-	<?php echo $content; ?>
-</div><!-- content -->
+<?php echo $content; ?>
 <?php $this->endContent(); ?>
