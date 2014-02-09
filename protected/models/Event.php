3653dc2f5858c9d85c9b318126cb0a25d68a3f6e
ligreman@gmail.com <ligreman@gmail.com@0186b76e-ba99-af01-771a-274165256fcf>
2014-02-09 21:50:07 +0000

    Parche de la 3.4 Final

--- protected/models/Event.php
+++ protected/models/Event.php
@@ -10,14 +10,8 @@
  * @property string $caller_side
  * @property integer $relauncher_id
  * @property integer $status
- * @property integer $gungubos_population
- * @property integer $gungubos_kafhe
  * @property integer $fame_kafhe
- * @property integer $gungubos_achikhoria
  * @property integer $fame_achikhoria
- * @property string $last_gungubos_criadores
- * @property integer $stored_tueste_kafhe
- * @property integer $stored_tueste_achikhoria
  * @property string $type
  * @property string $date
  */
@@ -50,13 +44,12 @@ class Event extends CActiveRecord
 		// will receive user inputs.
 		return array(
 			array('group_id, date', 'required'),
-			array('group_id, caller_id, relauncher_id, status, gungubos_population, gungubos_kafhe, fame_kafhe, gungubos_achikhoria, fame_achikhoria, stored_tueste_kafhe, stored_tueste_achikhoria', 'numerical', 'integerOnly'=>true),
+			array('group_id, caller_id, relauncher_id, status, fame_kafhe, fame_achikhoria', 'numerical', 'integerOnly'=>true),
 			array('caller_side', 'length', 'max'=>10),
 			array('type', 'length', 'max'=>8),
-			array('last_gungubos_criadores', 'safe'),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('id, group_id, caller_id, caller_side, relauncher_id, status, gungubos_population, gungubos_kafhe, fame_kafhe, gungubos_achikhoria, fame_achikhoria, last_gungubos_criadores, stored_tueste_kafhe, stored_tueste_achikhoria, type, date', 'safe', 'on'=>'search'),
+			array('id, group_id, caller_id, caller_side, relauncher_id, status, fame_kafhe, fame_achikhoria, type, date', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -83,14 +76,8 @@ class Event extends CActiveRecord
 			'caller_side' => 'Caller Side',
 			'relauncher_id' => 'Relauncher',
 			'status' => 'Status',
-			'gungubos_population' => 'Gungubos Population',
-			'gungubos_kafhe' => 'Gungubos Kafhe',
 			'fame_kafhe' => 'Fame Kafhe',
-			'gungubos_achikhoria' => 'Gungubos Achikhoria',
 			'fame_achikhoria' => 'Fame Achikhoria',
-			'last_gungubos_criadores' => 'Last Gungubos Criadores',
-			'stored_tueste_kafhe' => 'Stored Tueste Kafhe',
-			'stored_tueste_achikhoria' => 'Stored Tueste Achikhoria',
 			'type' => 'Type',
 			'date' => 'Date',
 		);
@@ -113,14 +100,8 @@ class Event extends CActiveRecord
 		$criteria->compare('caller_side',$this->caller_side,true);
 		$criteria->compare('relauncher_id',$this->relauncher_id);
 		$criteria->compare('status',$this->status);
-		$criteria->compare('gungubos_population',$this->gungubos_population);
-		$criteria->compare('gungubos_kafhe',$this->gungubos_kafhe);
 		$criteria->compare('fame_kafhe',$this->fame_kafhe);
-		$criteria->compare('gungubos_achikhoria',$this->gungubos_achikhoria);
 		$criteria->compare('fame_achikhoria',$this->fame_achikhoria);
-		$criteria->compare('last_gungubos_criadores',$this->last_gungubos_criadores,true);
-		$criteria->compare('stored_tueste_kafhe',$this->stored_tueste_kafhe);
-		$criteria->compare('stored_tueste_achikhoria',$this->stored_tueste_achikhoria);
 		$criteria->compare('type',$this->type,true);
 		$criteria->compare('date',$this->date,true);
 
