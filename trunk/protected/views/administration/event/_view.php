e992449f4d4620303dc644a5719f142f95353586
ligreman@gmail.com <ligreman@gmail.com@0186b76e-ba99-af01-771a-274165256fcf>
2014-02-09 21:50:07 +0000

    Parche de la 3.4 Final

--- trunk/protected/views/administration/event/_view.php
+++ trunk/protected/views/administration/event/_view.php
@@ -24,37 +24,13 @@
 	<?php echo CHtml::encode($data->status); ?>
 	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('gungubos_population')); ?>:</b>
-	<?php echo CHtml::encode($data->gungubos_population); ?>
-	<br />
-
-	<?php /*
-	<b><?php echo CHtml::encode($data->getAttributeLabel('gungubos_kafhe')); ?>:</b>
-	<?php echo CHtml::encode($data->gungubos_kafhe); ?>
-	<br />
-
 	<b><?php echo CHtml::encode($data->getAttributeLabel('fame_kafhe')); ?>:</b>
 	<?php echo CHtml::encode($data->fame_kafhe); ?>
 	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('gungubos_achikhoria')); ?>:</b>
-	<?php echo CHtml::encode($data->gungubos_achikhoria); ?>
-	<br />
-
+	<?php /*
 	<b><?php echo CHtml::encode($data->getAttributeLabel('fame_achikhoria')); ?>:</b>
 	<?php echo CHtml::encode($data->fame_achikhoria); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('last_gungubos_criadores')); ?>:</b>
-	<?php echo CHtml::encode($data->last_gungubos_criadores); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('stored_tueste_kafhe')); ?>:</b>
-	<?php echo CHtml::encode($data->stored_tueste_kafhe); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('stored_tueste_achikhoria')); ?>:</b>
-	<?php echo CHtml::encode($data->stored_tueste_achikhoria); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('type')); ?>:</b>
