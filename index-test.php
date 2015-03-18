b844b0b0fcda3ce16aeb2a67ccec110f3c2d241a
Ligre <ligreman@users.noreply.github.com>
2015-03-18 20:13:31 +0100

    Update index-test.php

--- index-test.php
+++ index-test.php
@@ -1,9 +1,5 @@
 <?php
-/**
- * This is the bootstrap file for test application.
- * This file should be removed when the application is deployed for production.
- */
- 
+
   //Prueba de edición
 
 // change the following paths if necessary
@@ -13,5 +9,6 @@ $config=dirname(__FILE__).'/protected/config/test.php';
 // remove the following line when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 
+//Requires
 require_once($yii);
 Yii::createWebApplication($config)->run();
