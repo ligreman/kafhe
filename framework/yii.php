44501dc4bcbb2f72e8308e7bff6337dc48dd876e
Ligre <ligreman@users.noreply.github.com>
2015-05-27 23:47:09 +0200

    New version framework

--- framework/yii.php
+++ framework/yii.php
@@ -2,26 +2,26 @@
 /**
  * Yii bootstrap file.
  *
- * @author Qiang Xue <qiang.xue@gmail.com>
  * @link http://www.yiiframework.com/
- * @copyright Copyright &copy; 2008-2011 Yii Software LLC
+ * @copyright Copyright (c) 2008 Yii Software LLC
  * @license http://www.yiiframework.com/license/
- * @package system
- * @since 1.0
  */
 
-require(dirname(__FILE__).'/YiiBase.php');
+require(__DIR__ . '/BaseYii.php');
 
 /**
  * Yii is a helper class serving common framework functionalities.
  *
- * It encapsulates {@link YiiBase} which provides the actual implementation.
- * By writing your own Yii class, you can customize some functionalities of YiiBase.
+ * It extends from [[\yii\BaseYii]] which provides the actual implementation.
+ * By writing your own Yii class, you can customize some functionalities of [[\yii\BaseYii]].
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
- * @package system
- * @since 1.0
+ * @since 2.0
  */
-class Yii extends YiiBase
+class Yii extends \yii\BaseYii
 {
 }
+
+spl_autoload_register(['Yii', 'autoload'], true, true);
+Yii::$classMap = require(__DIR__ . '/classes.php');
+Yii::$container = new yii\di\Container();
