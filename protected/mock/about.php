b17931915473682ed2254cbe17e0c91a25e5b779
Ligre <ligreman@users.noreply.github.com>
2018-06-28 17:27:55 +0200

    POC

---
+++ protected/mock/about.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+	<link href="main.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+    <title>Los Juegos del Kafhe</title>
+  </head>
+  <body>
+<p class="text-right">
+	<a href="index.php" role="button" style="margin: 10px;" class="btn btn-outline-dark">Volver</a>
+</p>
+
+<div class="container">
+<div class="row">
+	<div class="col-sm text-right">
+		    <h1>Sobre Kafhe</h1>
+
+			<p>Kafhe es un proyecto opensource desarrollado por los mismos creadores de "Desayuno" y "La Leyenda de Kafhe".</p>
+
+    <h1>Licencia</h1>
+
+    <p><a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Licencia Creative Commons"
+                                                                                      style="border-width:0"
+                                                                                      src="http://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br /><span
+            xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Kafhe</span> está distribuido bajo una <a style="color:blue"
+            rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Licencia Creative Commons
+            Atribución-NoComercial-SinDerivar 4.0 Internacional</a>.</p>
+
+    <h1>Colaboradores</h1>
+
+	<p>Participan en el proyecto, directa o indirectamente:</p>
+	
+		<p>Fernando García: Líder del proyecto</p>
+		<p>Paco: Programador y maquetador</p>
+	
+
+	</div>
+</div>
+<div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ protected/mock/index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+	<link href="main.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+    <title>Los Juegos del Kafhe</title>
+  </head>
+  <body>
+<p class="text-right">
+	<a href="about.php" role="button" style="margin: 10px;" class="btn btn-outline-dark">Acerca de...</a>
+</p>
+
+<div class="container">
+<div class="row">
+	<div class="col-sm text-right">
+		<img src="k256.png" />
+	</div>
+
+	<div class="col-sm">
+		<form action="login.php">
+		  <div class="form-group">
+			<label for="user">Usuario</label>
+			<input type="text" class="form-control" id="user">
+		  </div>
+		  <div class="form-group">
+			<label for="pass">Password</label>
+			<input type="password" class="form-control" id="pass">
+		  </div>
+		  <div class="text-center">
+			  <button type="submit" class="btn btn-success btn-lg btn-block">Entrar</button>
+		  </div>
+		</form>
+	</div>
+	<div class="col-sm"></div>
+</div>
+<div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
+</html>
