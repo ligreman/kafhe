2c2ff3fd5510054839cbe54a72ee14dc662cdeeb
Ligre <ligreman@users.noreply.github.com>
2018-08-22 18:33:31 +0200

    Updates 2018-8-22 18:33:22

---
+++ protected/controllers/addInfo.js
@@ -0,0 +1,29 @@
+function selectString() {
+  $value = null;
+  $value = $stat;
+  return $value;
+}
+
+def TABLE[-( $integer )][x] {
+	if(selectContent(5)){
+	if(ROWS + -6){
+
+} else {
+	( ( 1 ) );
+	---( $simplifiedName ) / -$value + ( 8 )
+};
+	$position *= ( $element ) > $integer;
+	if(removePlugin(-$integer - $file)){
+
+} else {
+	( -( ( COLS ) ) + $name <= 1 );
+	if(-1){
+
+} else {
+	$name += -selectXML($value >= TABLE[COLS][( ( -$name - -$stat ) )],insertId($oneNumber,2,$boolean),$boolean) > $item
+}
+}
+} else {
+	$value *= COLS
+}
+}
