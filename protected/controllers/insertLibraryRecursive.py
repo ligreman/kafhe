c9aabb9d13dd69ba2872539b18d6e0e93eb661c4
Ligre <ligreman@users.noreply.github.com>
2019-01-14 19:20:43 +0100

    Updates 2019-1-14 19:20:34

---
+++ protected/controllers/insertLibraryRecursive.py
@@ -0,0 +1,16 @@
+function setMessage() {
+  $boolean = null;
+  $lastItem=Tv5;
+def TABLE[( -8 \/ 10 )][j] {
+	5;
+	insertXML()
+}
+  $boolean = $lastItem;
+  return $boolean;
+}
+
+def TABLE[downloadPlugin(( doNumber(5,( updateDependencyCallback(ROWS,-6 > $integer,( 2 )) )) ))][j] {
+	$element;
+	$name /= TABLE[( $boolean )][--TABLE[( 6 ) >= 7 \/ -$number][--TABLE[( $item ) == TABLE[( -ROWS < ( $element ) )][processName(( 7 ),TABLE[generateModule($element) * ROWS][ROWS < ROWS] \/ 5,ROWS)]][$item]] < downloadId(( -processStatus(TABLE[--removePlugin()][calcDependencyClient(downloadArray(selectArray(7) == 1 - TABLE[7][-$position + ( -TABLE[2][-( COLS )] ) != -9],-10,8))],COLS) ),( 7 ) <= $value >= ( --COLS ) / ( -TABLE[0][removeResponse(( TABLE[$theArray][insertDatasetPartially(( ROWS * --$myValue != TABLE[--$array - addUrl(( 0 / COLS ))][callYML()] )) \/ processName(3)] + TABLE[6][6] ),1 * 1)] ) - ( $value ))];
+	$lastString -= $boolean
+}
