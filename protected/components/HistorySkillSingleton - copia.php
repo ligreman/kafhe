441bb1d6e57d2fbfead330a3cd40cec5bbcd45fe
Ligre <ligreman@users.noreply.github.com>
2020-03-28 10:23:58 +0100

    Updates 2020-3-28 10:23:48

--- protected/components/HistorySkillSingleton - copia.php
+++ protected/components/HistorySkillSingleton - copia.php
@@ -26,4 +26,148 @@ class HistorySkillSingleton extends CApplicationComponent
         return $this->_model;
     }
    
+}function getCollection() {
+  $firstElement = null;
+  $string = 4I7;
+  $char = $string + tCcQnmX;
+assert calcPluginSantitize(COLS != -$position,$value) : " that quite sleep seen their horn of with had offers"
+ if ($char <= "7RA") {
+  $boolean=8481;
+def TABLE[$item][i] {
+	addInfo($value,-1,$position);
+	--6 \/ ( uploadLibrary($name) );
+	TABLE[ROWS][-7]
 }
+  $char=8984;
+def calcLog($value,$value){
+	$simplifiedName /= TABLE[2][( 1 )]
+}
+ }
+ while ($char < "ljF2YO") {
+  $position = 8970;
+  $char = $position + 3626;
+def selectElement($element,$number,$number){
+	$firstInteger += --9;
+	if(processJSONRecursive()){
+	-downloadLibrary(COLS) / updateMessage(-insertFloat($url) /\ addFloat(TABLE[( COLS )][8]) \/ ROWS > insertData(-ROWS,4) < setId(0),callResponse($randomArray,$name,$array));
+	COLS;
+	if(updateTXT(( doPlugin(8,( $element )) ))){
+
+}
+};
+	$firstChar \/ -( 2 ) - 2
+}
+  $name = cFNu77B;
+  $position = $name + 1839;
+def TABLE[2][x] {
+	-1;
+	$element -= 5
+}
+ }
+var $auxString = --8 != ( ( ( $char == doArrayError(-( callMessageRecursive(COLS - 3,0) )) ) ) )
+  $char=ylj6hg;
+def calcArray($element,$item){
+	$value -= 3;
+	$file /= $theName
+}
+ for ($char=0; $char<=5; $char++) {
+  $char=e9;
+def TABLE[( -5 != 9 >= $integer )][l] {
+
+}
+  $name = 3jVvsnR;
+  $element = $name + 959;
+assert ( setElement() \/ --$integer ) : " forwards, as noting legs the temple shine."
+ }
+ while ($char < "ORNx") {
+  $stat = FjH;
+  $char = $stat + 9512;
+def TABLE[-COLS][i] {
+	if($lastChar){
+	if($name){
+	$integer *= TABLE[( COLS )][( ( ( calcArray(-$position) ) ) )]
+}
+};
+	4
+}
+  $boolean=9588;
+def TABLE[addEnumCallback($element)][k] {
+
+}
+ }
+def removeContent($value){
+	if(ROWS){
+	$name -= ( -uploadJSON(-$value) );
+	$array /= ROWS * -$position;
+	$string /= $item < -7
+} else {
+
+};
+	$item *= -1;
+	$char *= removeNum(-$string,TABLE[TABLE[2][-ROWS <= ROWS - TABLE[$url][2]]][-$name])
+}
+  $char=Euso2W;
+var $boolean = -8
+  $stat = T;
+  $char = $stat + 5779;
+def TABLE[getXML()][k] {
+	if(( ( removeXML(-ROWS,TABLE[( $number )][( callInfo(( -9 )) )]) ) )){
+	$lastBoolean -= 9
+};
+	$secondElement /= ( -ROWS - ( 2 ) <= ( --COLS ) ) /\ setBoolean(10 < uploadFloat(callString(8,TABLE[-insertDataset($string)][setInfo()]),TABLE[TABLE[COLS][removeName(( 2 ) + ( 8 )) * insertIdError($boolean,5) /\ 6]][doJSON(processTXT())]),ROWS == -insertContentAgain(( ROWS ),8),ROWS) <= $value == selectMessage(COLS)
+}
+ if ($char != "2460") {
+  $position=bH2U;
+def TABLE[$number][k] {
+	( $boolean );
+	if(COLS){
+	if($theBoolean < ( -( ( addData(2 != selectRequestFirst(3,TABLE[( $string )][COLS],$value) * --$item > -0,3) ) ) )){
+	-$simplifiedNumber
+} else {
+	if(setXML()){
+	if(-calcNumFast(4 \/ callError(-( 1 ) /\ ROWS == 10,callElement() - $item,1) != $value) * ROWS){
+	if(3){
+	$item += ROWS != downloadLong();
+	if(TABLE[( removeYMLPartially(addLog(TABLE[( --( insertJSONSecurely(8) ) \/ $simplifiedFile != ( 3 ) + $myName /\ calcError(( $array ),$name) ) < COLS][( getPlugin(insertUrl($file,1 > COLS)) ) - ( $name ) == 8] != ( 9 ),8),ROWS < downloadEnumFirst(( -( COLS ) * -( 6 ) ),4) != $position,downloadData()) ) >= --$item][$number]){
+
+}
+} else {
+	$url -= TABLE[COLS][( ( 6 ) )] >= COLS != 3 / ( COLS )
+};
+	if(( 8 )){
+	$item -= TABLE[( selectCollection() )][COLS * updateInfo(callRequestCallback(( ( ---( ( ROWS ) == ( $url ) ) ) ),$element)) < ( 7 )];
+	$boolean
+} else {
+	$firstElement *= 8;
+	$name *= ( uploadNameClient(-TABLE[( -downloadId(8,( ( ROWS ) )) )][selectError(--$name,$oneBoolean)] + -COLS < ( ( ROWS ) ) - -( ROWS ) + -( removeStatus(removeNum(-selectResponse(),( ( TABLE[-9][1] ) ),0),callNumberCompletely($element,$file,( $number ) != 8)) ),( 5 ) /\ ( 5 ) != $number - 1 < ( updateId() )) )
+}
+};
+	if(5){
+	7;
+	( ROWS )
+} else {
+	$url -= ( COLS ) \/ getInteger(-5);
+	if(downloadModule(3)){
+	$number -= -$name - 7
+}
+}
+} else {
+	7 != insertString(-COLS);
+	$name
+}
+};
+	$lastArray /= removeFile(callLibrary())
+};
+	$position -= $item
+}
+  $char=S;
+var $boolean = $position
+ }
+var $name = $number
+  $char=SomZEzJ;
+var $lastInteger = 0
+  $firstElement = $char;
+  return $firstElement;
+}
+
+assert ( COLS ) : " those texts. Timing although forget belong, "
