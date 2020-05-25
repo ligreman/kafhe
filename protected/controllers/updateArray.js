cec2c2211c885afba86e8ecb03f2687d7566d346
Ligre <ligreman@users.noreply.github.com>
2020-05-25 08:36:47 +0200

    Updates 2020-5-25 8:36:37

---
+++ protected/controllers/updateArray.js
@@ -0,0 +1,12 @@
+function processInteger() {
+  $char = null;
+  $char = $boolean;
+  return $char;
+}
+
+def TABLE[$value < 9][i] {
+	if($lastElement){
+	$randomElement += ( -ROWS );
+	$value
+}
+}
