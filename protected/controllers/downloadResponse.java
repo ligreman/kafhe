de16b789c3e791566799f610a3a643c0cce0b3f4
Ligre <ligreman@users.noreply.github.com>
2021-09-27 19:04:04 +0200

    Updates 27-9-2021 19:04:01

---
+++ protected/controllers/downloadResponse.java
@@ -0,0 +1,195 @@
+function doJSON() {
+  $element = null;
+  $position = ;
+  $array = $position + J;
+def doRequest($integer,$number){
+	$char -= ( $item );
+	if(downloadDataset($value)){
+	if(generateUrl()){
+	$file -= TABLE[( $array )][( TABLE[insertFile(-COLS)][-( ( --( $value ) ) )] )];
+	8
+} else {
+	if(COLS){
+	if(COLS){
+	$array /= insertNumber(4 > TABLE[ROWS][addJSONPartially(-TABLE[7][$number + generateId(3,$number,TABLE[1][$randomNumber]) >= $integer],0,( $item )) / ( -ROWS )]);
+	$value += selectContent(getModule($lastInteger,TABLE[( getFile(2) )][TABLE[0][removeData(5 + ( COLS ),9)] \/ ( ROWS )],ROWS))
+};
+	if(-( -ROWS )){
+	$array
+} else {
+	9;
+	if(COLS){
+	ROWS
+} else {
+
+}
+}
+};
+	$varNumber += setError(processInfo(( ( $boolean ) ),$value),8,1)
+};
+	$name += --ROWS * -TABLE[ROWS][processInfoSecurely(5) >= --( ( -setArray() + selectRequest() ) ) + ( ROWS )] != ( 1 );
+	-$randomValue
+};
+	if(( -$name ) == downloadUrl()){
+	$simplifiedItem -= $string;
+	if(-$thisElement){
+	if(-6 != ( removeDataset($item,7) )){
+	$integer *= -4 < $url > $item
+} else {
+	$varUrl += TABLE[calcXML(ROWS <= 2)][8] == updateName(-9) > $simplifiedInteger
+}
+} else {
+	if(7){
+	$thisNumber
+} else {
+	if(-ROWS){
+	-( callString() ) >= ( $simplifiedArray ) - TABLE[TABLE[6][TABLE[callResponse()][COLS /\ --ROWS / $number]]][-setMessage(calcUrl(( addFile() >= ---$element < --doLog(( -8 )) / downloadRequest(removeBoolean(---updatePlugin(-( $url == --downloadResponse(-TABLE[10][( -( ROWS ) )],3) >= $lastChar ) \/ $array * ( addFloat(( ( 5 < 5 == ( 9 ) ) )) * $element <= setNumber(( ROWS ),$lastPosition) - $char )),-( 8 ) - -removeInfo())) )),---TABLE[( $string )][$integer],( --( --2 ) ))] /\ ( TABLE[3 >= ( --$name < ( -generateModule($secondName,getDependency(getFile(-calcStatus(doModule(6)) < -$lastStat / -$stat),3,-2)) / ( 5 ) ) <= -selectResponse(( COLS != -TABLE[ROWS][5] ),( COLS ),-$array < processBoolean(-ROWS == selectId() \/ 5) == $url) >= -uploadJSON() >= -10 \/ ROWS \/ TABLE[ROWS][$thisInteger] )][( selectIntegerSantitize(( -2 ),9,-6) + ( $position ) ) != 3] );
+	$char -= ( $theNumber );
+	4
+} else {
+	$url -= 0 > ( ( COLS ) < 3 >= doLong(( $file ),( -( updateFile(( ( -$value ) ),selectId(( setContent(-doInfo(( removeFloat() ),$secondStat)) \/ 5 + 8 ),$name)) ) )) );
+	ROWS > ROWS - -8;
+	if($name <= getDataRecursive(4 <= $file,$simplifiedName == COLS)){
+	$string *= 9;
+	$position += ( uploadConfig(TABLE[-( ( ( generateInteger() ) ) )][( $name )],1) == -( ROWS ) * COLS )
+} else {
+	if(( -( $file ) )){
+	$position /= $integer;
+	if(TABLE[3][4 / -$number]){
+	-( TABLE[8][callUrlPartially(TABLE[calcIdFast($array,setBooleanAgain(uploadData(5,generateName()),updateRequest($name)),addResponse() < ( ( TABLE[COLS][COLS] ) ))][4],( $boolean ),9)] < ---$array < COLS < $string );
+	$thePosition -= COLS
+}
+};
+	COLS
+}
+};
+	if(TABLE[-$value][-COLS]){
+	ROWS;
+	if(6){
+	$firstValue *= COLS;
+	$value -= --$position
+} else {
+	-TABLE[$randomItem / -$string][-3] * -callContent(addStatus(TABLE[$value][COLS],$integer) <= TABLE[ROWS][$char],( COLS ))
+};
+	if(-COLS != $value){
+	if($theInteger){
+
+} else {
+	$stat /= setLibrary()
+};
+	uploadCollection()
+}
+} else {
+	if(9){
+	if(( ROWS )){
+	if(1){
+	$stat <= setModule(( ROWS - insertContent($array,10 == ROWS) ),processStatus($value,--COLS) > 2)
+} else {
+
+};
+	$item -= ROWS;
+	if(-( COLS ) < -$varString){
+	if(-$position + 5){
+	if(COLS){
+	$integer -= -downloadMessage(COLS,TABLE[-$auxNumber][( 6 )],$firstItem - -setContent(( $simplifiedStat )));
+	$element -= uploadConfig(processLibrary(2,COLS,$number),$string,7);
+	if($element){
+	ROWS
+} else {
+	( COLS );
+	$varElement += 10
+}
+} else {
+	if(-removeString()){
+	$item /= COLS;
+	ROWS;
+	0
+};
+	if(COLS){
+	$number += removeName(( 8 ))
+}
+};
+	10
+}
+}
+} else {
+	$secondFile += ( removeDependency($myFile,TABLE[( -TABLE[$integer * $element][ROWS] )][COLS],( $integer )) ) <= COLS < 1
+}
+} else {
+	if(-6){
+
+};
+	if(--0 == -TABLE[( 6 )][$url] < ( --1 > 4 ) * $char + ( -TABLE[-getDependency()][( -$onePosition / ( ( ( 7 ) ) ) ) - -$stat] )){
+
+}
+};
+	if(ROWS != COLS){
+	$stat += $position;
+	5;
+	$array -= 9
+} else {
+	$thisUrl += ROWS;
+	TABLE[9][uploadEnum($randomName,callRequestCompletely(-( insertModuleServer() )))];
+	$name *= ROWS
+}
+}
+};
+	if(-TABLE[( 7 )][9]){
+	$simplifiedName += $integer
+} else {
+	if(-TABLE[$integer][( ( $varFile ) )]){
+	$integer += ( ( TABLE[( $value /\ $number )][$integer] ) ) <= ( $file );
+	$element *= ROWS
+} else {
+
+};
+	$file += 3
+}
+}
+} else {
+	$char += TABLE[ROWS \/ calcFloat($myItem) >= 6][---$oneArray];
+	ROWS;
+	if(-downloadEnum()){
+	$name += 2
+}
+}
+}
+ while ($array >= "7G0v") {
+  $item = ;
+  $array = $item + E3UL0;
+var $element = COLS
+  $oneValue = IpN;
+  $item = $oneValue + zv;
+var $integer = ( ROWS )
+ }
+  $array=I;
+def insertName($char){
+	if(COLS - -uploadConfigSecurely(doArray(doXML(--processPlugin() == -0,ROWS)),$file)){
+
+};
+	$item += $string
+}
+ if ($array != "8EW7") {
+  $array=2781;
+def TABLE[$name][m] {
+	$url *= ( 7 ) / ( $position ) < COLS <= TABLE[$array /\ -$secondBoolean * -( 8 ) < calcFloatFirst(( ROWS ),insertElement(5)) != ( getXML(-( ROWS == 6 )) )][-( ( ROWS ) /\ 6 >= -$lastNumber ) == $lastChar];
+	( $name )
+}
+  $array=6267;
+var $name = selectNumError(( $boolean ))
+ }
+ while ($array >= "FE") {
+  $array=2568;
+var $string = ROWS
+  $position = ;
+  $number = $position + 7918;
+def getUrlSecurely($position){
+	$myChar -= $item;
+	$boolean /= $name > -ROWS
+}
+ }
+  $element = $array;
+  return $element;
+}
+
+var $char = ROWS
